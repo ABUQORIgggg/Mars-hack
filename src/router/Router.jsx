1d845161fa98d1fcd67de62fcf5492d1d556f9db
MukhamedovK <kamoliddin2619@gmail.com>
2024-09-22 13:28:38 +0500

    ASD

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -3,6 +3,8 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Loadable from "../components/Loadable/Loadable";
 import Blog from "../pages/Blog/Blog";
 import Shop from "../pages/Shop/Shop";
+import Login from "../pages/Login/Login";
+import PrivateRoute from "../components/PrivateRoute/PrivateRoute"; // Import PrivateRoute
 
 const App = Loadable(lazy(() => import("../App")));
 const Home = Loadable(lazy(() => import("../pages/Home/Home")));
@@ -11,21 +13,37 @@ const RouterConfig = () => {
   const router = createBrowserRouter([
     {
       path: "/",
-      element: <App/>,
+      element: <App />,
       children: [
         {
           path: "/main",
-          element: <Home />,
+          element: (
+            <PrivateRoute>
+              <Home />
+            </PrivateRoute>
+          ),
         },
         {
           path: "/blog",
-          element: <Blog />,
+          element: (
+            <PrivateRoute>
+              <Blog />
+            </PrivateRoute>
+          ),
         },
         {
           path: "/space-shop",
-          element: <Shop />,
+          element: (
+            <PrivateRoute>
+              <Shop />
+            </PrivateRoute>
+          ),
         },
       ],
+    },
+    {
+      path: "/login",
+      element: <Login />,
     },
   ]);
 
